9800ad8bdd3b91d9b35e55fd7985a86fbc55a1d4
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-01-31 18:36:55 -0500

    commit

--- src/main/java/com/steamrankings/service/core/Application.java
+++ src/main/java/com/steamrankings/service/core/Application.java
@@ -1,13 +1,28 @@
 package com.steamrankings.service.core;
 
-/**
- * Hello world!
- *
- */
-public class Application 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Application {
+	public static void main(String[] args) throws IOException {
+		int port = 6789;
+
+		ServerSocket serverSocket;
+		serverSocket = new ServerSocket(port);
+
+		while (true) {
+			Socket clientSocket = serverSocket.accept();
+			RequestHandler requestHandler;
+			
+			try {
+				requestHandler = new RequestHandler(clientSocket);
+			} catch (Exception e) {
+				break;
+			}
+			new Thread(requestHandler).start();
+		}
+
+		serverSocket.close();
+	}
 }
