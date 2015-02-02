ed8cd751f9b12536c8dad031cd4e3590a956589d
Michael Smith <michael.smith6@mail.mcgill.ca>
2015-02-01 21:59:02 -0500

    Set deployment only on master

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'travis_test' ]]
-  cd test/dummy
-  rake db:schema:load
-else
-  cd spec/dummy
-  rake db:schema:load
+if [[ $TRAVIS_BRANCH == 'master' ]]
+	mvn deploy -Papi -B -V -s settings.xml
 fi
