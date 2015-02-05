b84424636e8f763789f79379d523adcc3d5c2c0d
Ossama Ahmed <ossama.ahmed@mail.mcgill.ca>
2015-02-05 18:31:39 -0500

    Added games, achievments and leaderboard api

--- src/main/java/com/steamrankings/service/api/leaderboards/Leaderboards.java
+++ src/main/java/com/steamrankings/service/api/leaderboards/Leaderboards.java
@@ -1,9 +1,53 @@
 package com.steamrankings.service.api.leaderboards;
 
+import java.io.InputStream;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.steamrankings.service.api.games.SteamGame;
 
 public class Leaderboards {
     public static List<RankEntryByAchievements> getRanksByAchievementTotal(int fromRank, int toRank) {
-        return null;
+    	
+   	 HttpClient client = new DefaultHttpClient();
+     HttpGet request = new HttpGet("http://mikemontreal.ignorelist.com:6789/ranksbyachievments?fromrank=" + fromRank +"&torank=" + toRank);
+     HttpResponse response = null;
+
+     try {
+         response = client.execute(request);
+     } catch (Exception e) {
+         return null;
+     }
+
+     HttpEntity entity = response.getEntity();
+     InputStream is = null;
+     try {
+         is = entity.getContent();
+     } catch (Exception e) {
+         return null;
+     }
+
+     ObjectMapper mapper = new ObjectMapper();
+     RankEntryByAchievementListWrapper rankByAchievements = null;
+
+     try {
+    	 rankByAchievements = mapper.readValue(is, RankEntryByAchievementListWrapper.class);
+     } catch (Exception e) {
+         return null;
+     }
+
+     try {
+         is.close();
+     } catch (Exception e) {
+         return null;
+     }
+
+     return rankByAchievements.getRankEntryByAchievements();
     }
 }
---
+++ src/main/java/com/steamrankings/service/api/leaderboards/RankEntryByAchievementListWrapper.java
@@ -0,0 +1,13 @@
+package com.steamrankings.service.api.leaderboards;
+
+import java.util.List;
+
+public class RankEntryByAchievementListWrapper {
+	
+	  private List<RankEntryByAchievements> rankEntries;                             
+      
+	    public List<RankEntryByAchievements> getRankEntryByAchievements() {                        
+	        return this.rankEntries;                                               
+	    }       
+
+}
