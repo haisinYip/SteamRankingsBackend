038f0789babbc344b9380619bc8a4cb37c0ab60f
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-06 03:16:06 -0500

    Removed references to WebApi

--- src/main/java/com/steamrankings/service/core/Application.java
+++ src/main/java/com/steamrankings/service/core/Application.java
@@ -7,28 +7,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
 
-import com.github.koraktor.steamcondenser.exceptions.WebApiException;
-import com.github.koraktor.steamcondenser.steam.community.WebApi;
-
 public class Application {
     final public static Properties CONFIG = new Properties();
-    
+
     public static void main(String[] args) throws IOException {
         InputStream inputStream = new FileInputStream("config.properties");
         CONFIG.load(inputStream);
         inputStream.close();
-        
+
         int port = Integer.parseInt(CONFIG.get("server_port").toString());
 
         ServerSocket serverSocket;
         serverSocket = new ServerSocket(port);
-
-        try {
-            WebApi.setApiKey(CONFIG.getProperty("apikey"));
-        } catch (WebApiException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
