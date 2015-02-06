ba7fa319c035b9d90bf8ef2ce1cd901ce4a5bc1d
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-06 03:20:17 -0500

    Some formatting fixes

--- src/main/java/com/steamrankings/service/api/leaderboards/RankEntry.java
+++ src/main/java/com/steamrankings/service/api/leaderboards/RankEntry.java
@@ -1,30 +1,32 @@
 package com.steamrankings.service.api.leaderboards;
 
 public abstract class RankEntry {
-	
-	private String rankNumber;
-	private String name;
-	private String id64;
-	
-	
-    public  String getRankNumber(){
-    	return this.rankNumber;
-    }
-    public void setRankNumber(String rankNumber){
-    	this.rankNumber=rankNumber;
+
+    private String rankNumber;
+    private String name;
+    private String id64;
+
+    public String getRankNumber() {
+        return this.rankNumber;
     }
 
-    public  String getName(){
-    	return this.name;
-    }
-    public  void setName(String name){
-    	this.name=name;
+    public void setRankNumber(String rankNumber) {
+        this.rankNumber = rankNumber;
     }
 
-    public  String getId64(){
-    	return this.id64;
+    public String getName() {
+        return this.name;
     }
-    public  void setId64(String id64){
-    	this.id64=id64;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId64() {
+        return this.id64;
+    }
+
+    public void setId64(String id64) {
+        this.id64 = id64;
     }
 }
--- src/main/java/com/steamrankings/service/api/leaderboards/RankEntryByAchievements.java
+++ src/main/java/com/steamrankings/service/api/leaderboards/RankEntryByAchievements.java
@@ -1,29 +1,32 @@
 package com.steamrankings.service.api.leaderboards;
 
 public abstract class RankEntryByAchievements extends RankEntry {
-	
-	private String totalNumberOfAchievments;
-	private String AverageCompetionRate;
-	private String country;
-	
-    public  String getTotalNumberOfAchievements(){
-    	return this.totalNumberOfAchievments;
-    }
-    public  void setTotalNumberOfAchievements(String totalNumberOfAchievments){
-    	this.totalNumberOfAchievments=totalNumberOfAchievments; 
+
+    private String totalNumberOfAchievments;
+    private String AverageCompetionRate;
+    private String country;
+
+    public String getTotalNumberOfAchievements() {
+        return this.totalNumberOfAchievments;
     }
 
-    public  String getAverageCompetionRate(){
-    	return this.AverageCompetionRate;
-    }
-    public  void setAverageCompetionRate(String AverageCompetionRate){
-    	this.AverageCompetionRate= AverageCompetionRate;
+    public void setTotalNumberOfAchievements(String totalNumberOfAchievments) {
+        this.totalNumberOfAchievments = totalNumberOfAchievments;
     }
 
-    public  String getCountry(){
-    	return this.country;
+    public String getAverageCompetionRate() {
+        return this.AverageCompetionRate;
     }
-    public  void setCountry(String country){
-    	this.country=country;
+
+    public void setAverageCompetionRate(String AverageCompetionRate) {
+        this.AverageCompetionRate = AverageCompetionRate;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
