146f79b1035901adc37ae8f193cc9b6a684c4905
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-07 17:26:11 -0500

    Get games implemented

--- src/main/java/com/steamrankings/service/core/RequestHandler.java
+++ src/main/java/com/steamrankings/service/core/RequestHandler.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -12,8 +14,12 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONArray;
+
 import com.steamrankings.service.api.achievements.GameAchievement;
 import com.steamrankings.service.api.games.SteamGame;
+import com.steamrankings.service.api.leaderboards.RankEntryByAchievements;
 import com.steamrankings.service.api.profiles.SteamProfile;
 import com.steamrankings.service.database.DBConnector;
 import com.steamrankings.service.steam.SteamApi;
@@ -28,6 +34,11 @@ public class RequestHandler implements Runnable {
 
     private Socket socket;
     private static final Logger logger = Logger.getLogger(RequestHandler.class.getName());
+    private static final Object PARAMETER_LEADERBOARD_TYPE = "type";
+    private static final Object PARAMETER_TO_RANK = "to";
+    private static final Object PARAMETER_FROM_RANK = "from";
+    private static final Object REST_API_INTERFACE_LEADERBOARDS = "/leaderboards";
+    private static final Object REST_API_INTERFACE_GAMES = "/gamesowned";
 
     public RequestHandler(Socket socket) throws Exception {
         this.socket = socket;
@@ -73,6 +84,10 @@ public class RequestHandler implements Runnable {
     private void processGet(String restInterface, HashMap<String, String> parameters) throws IOException {
         if (restInterface.equals(REST_API_INTERFACE_PROFILES)) {
             processGetProfiles(parameters);
+        } else if (restInterface.equals(REST_API_INTERFACE_LEADERBOARDS)) {
+            processGetLeaderboards(parameters);
+        } else if (restInterface.equals(REST_API_INTERFACE_GAMES)) {
+            processGetGames(parameters);
         }
     }
 
@@ -114,6 +129,84 @@ public class RequestHandler implements Runnable {
         db.closeConnection();
 
         return;
+    }
+
+    private void processGetGames(HashMap<String, String> parameters) throws IOException {
+        // Check to see if parameters are correct
+        if (parameters == null || parameters.isEmpty()) {
+            sendResponse(socket, "HTTP/1.1 400" + CRLF, "Content-type : " + "text/plain" + CRLF, "Invalid parameters");
+            return;
+        }
+
+        DBConnector db = new DBConnector();
+
+        if (parameters.containsKey("id")) {
+            String sql = "SELECT games_id FROM profiles_has_games WHERE profiles_id=" + Long.toString(Long.parseLong(parameters.get("id")) - SteamProfile.BASE_ID_64);
+            ResultSet results = db.queryDB(sql);
+            ArrayList<Integer> gameIds = new ArrayList<Integer>();
+            try {
+                results.first();
+                do {
+                    gameIds.add(results.getInt(1));
+                    results.next();
+                } while (!results.isAfterLast());
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            results = db.getTable("games");
+            ArrayList<SteamGame> games = new ArrayList<SteamGame>();
+            try {
+                results.first();
+                do {
+                    System.out.println(results.getInt("id"));
+                    if (gameIds.contains(results.getInt("id"))) {
+                        games.add(new SteamGame(results.getInt("id"), results.getString("icon_url"), results.getString("logo_url"), results.getString("name")));
+                    }
+                    results.next();
+                } while (!results.isAfterLast());
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            db.closeConnection();
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(games);
+            //JSONArray gamesJson = new JSONArray(games);
+            sendResponse(socket, "HTTP/1.1 200" + CRLF, "Content-type: " + "application/json" + CRLF, json);
+        } else if (parameters.containsKey("appId")) {
+
+        } else {
+
+        }
+    }
+
+    private void processGetLeaderboards(HashMap<String, String> parameters) throws IOException {
+        // Check to see if parameters are correct
+        if (parameters == null || parameters.isEmpty() || !parameters.containsKey(PARAMETER_LEADERBOARD_TYPE) || !parameters.containsKey(PARAMETER_TO_RANK)
+                || !parameters.containsKey(PARAMETER_FROM_RANK)) {
+            sendResponse(socket, "HTTP/1.1 400" + CRLF, "Content-type : " + "text/plain" + CRLF, "Invalid parameters");
+            return;
+        }
+
+        if (parameters.get(PARAMETER_LEADERBOARD_TYPE).equals("achievements")) {
+            DBConnector db = new DBConnector();
+            ArrayList<RankEntryByAchievements> leaderboard = processGetAchievementLeaderboard(db, parameters.get(PARAMETER_TO_RANK), parameters.get(PARAMETER_FROM_RANK));
+            if (leaderboard == null) {
+                sendResponse(socket, "HTTP/1.1 400" + CRLF, "Content-type: " + "text/plain" + CRLF, "Something went wrong");
+                db.closeConnection();
+                return;
+            } else {
+                sendResponse(socket, "HTTP/1.1 200" + CRLF, "Content-type: " + "application/json" + CRLF, leaderboard.toString());
+                db.closeConnection();
+                return;
+            }
+        }
+    }
+
+    private ArrayList<RankEntryByAchievements> processGetAchievementLeaderboard(DBConnector db, String string, String string2) {
+        return null;
     }
 
     private void processNewUser(DBConnector db, SteamDataExtractor steamDataExtractor, SteamProfile profile) {
