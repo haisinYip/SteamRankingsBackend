29d9613bca7b67c5793c969e4c5224fce5910784
Ossama Ahmed <ossama.ahmed@mail.mcgill.ca>
2015-02-08 18:15:30 -0500

    added the client implementation to the leaderboard

--- src/main/java/com/steamrankings/service/api/leaderboards/Leaderboards.java
+++ src/main/java/com/steamrankings/service/api/leaderboards/Leaderboards.java
@@ -11,15 +11,13 @@ import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONArray;
 
+import com.steamrankings.service.api.client.SteamRankingsClient;
+
 public class Leaderboards {
-    public static List<RankEntryByAchievements> getRanksByAchievementTotal(int fromRank, int toRank) {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("http://localhost:6789/leaderboards?type=achievements&from=" + fromRank + "&to=" + toRank);
-        HttpResponse response = null;
+    public static List<RankEntryByAchievements> getRanksByAchievementTotal(int fromRank, int toRank, SteamRankingsClient client) {
 
         try {
-            response = client.execute(request);
-            String data = EntityUtils.toString(response.getEntity());
+        	String data=client.excecuteRequest("leaderboards?type=achievements&from=" + fromRank + "&to=" + toRank);
 
             ObjectMapper mapper = new ObjectMapper();
             JSONArray jsonArray = new JSONArray(data);
