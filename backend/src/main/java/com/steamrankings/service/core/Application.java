5d6e6751af77a8b28e90bc83ddbdee58711471fc
Michael Smith <michael.smith6@mail.mcgill.ca>
2015-02-14 11:48:17 -0500

    Fix unicode bug

--- backend/src/main/java/com/steamrankings/service/core/Application.java
+++ backend/src/main/java/com/steamrankings/service/core/Application.java
@@ -8,29 +8,36 @@ import java.net.Socket;
 import java.util.Properties;
 
 public class Application {
-    final public static Properties CONFIG = new Properties();
+	final public static Properties CONFIG = new Properties();
 
-    public static void main(String[] args) throws IOException {
-        InputStream inputStream = new FileInputStream("config.properties");
-        CONFIG.load(inputStream);
-        inputStream.close();
+	public static void main(String[] args) throws IOException {
+		
+		// Load configuration file
+		InputStream inputStream = new FileInputStream("config.properties");
+		CONFIG.load(inputStream);
+		inputStream.close();
+		
+		// Open socket to receive requests from frontend
+		ServerSocket serverSocket = new ServerSocket(Integer.parseInt(CONFIG
+				.get("server_port").toString()));
+		System.out.println("Backend now running");
+		
+		
+		while (true) {
+			// Receive requests and handle them
+			Socket clientSocket = serverSocket.accept();
+			RequestHandler requestHandler;
 
-        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(CONFIG.get("server_port").toString()));
-        System.out.println("Backend now running");
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            RequestHandler requestHandler;
+			try {
+				System.out.println("New request received");
+				requestHandler = new RequestHandler(clientSocket);
 
-            try {
-                System.out.println("New request received");
-                requestHandler = new RequestHandler(clientSocket);
+			} catch (Exception e) {
+				break;
+			}
+			new Thread(requestHandler).start();
+		}
 
-            } catch (Exception e) {
-                break;
-            }
-            new Thread(requestHandler).start();
-        }
-
-        serverSocket.close();
-    }
+		serverSocket.close();
+	}
 }
