f3dce484380df2851fdd6a0153396c5ba8265425
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-28 15:48:32 -0500

    Fixed the API call to get all the steam games

--- api/src/main/java/com/steamrankings/service/api/games/Games.java
+++ api/src/main/java/com/steamrankings/service/api/games/Games.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -20,10 +15,6 @@ import com.steamrankings.service.api.SteamRankingsClient;
 import com.steamrankings.service.api.leaderboards.RankEntryByTotalPlayTime;
 
 public class Games {
-	 private static final String API_ERROR_BAD_ARGUMENTS_CODE = "1000";
-	 private static final String API_ERROR_STEAM_USER_DOES_NOT_EXIST = "2000";
-	 private static final String API_ERROR_STEAM_ID_INVALID = "3000";
-	
     public static SteamGame getSteamGame(int appId, SteamRankingsClient client) throws APIException, JsonParseException, JsonMappingException, IOException {
         String data = client.excecuteRequest("games?appId=" + appId);
 
@@ -45,78 +36,22 @@ public class Games {
 
         return games;
     }
-    
-    public static List<SteamGame> getSteamGames(int appid) throws Exception {
 
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("http://localhost:6789/games");
-        HttpResponse response = null;
-
-        try {
-            response = client.execute(request);
-            		
-            String data = EntityUtils.toString(response.getEntity());
-            
-            if (response.getStatusLine().getStatusCode() == 400) {
-            	if (data.equals(API_ERROR_STEAM_ID_INVALID)) {
-            		throw new APIException("Invalid or private Steam ID");
-            	}
-            	
-            	else if (data.equals(API_ERROR_BAD_ARGUMENTS_CODE)) {
-            		throw new APIException("Bad arguments passed to API");
-            	}
-            	else {
-            		throw new APIException("Unknown API exception; HTTP 400 error");
-            	}
-            }
-            else if (response.getStatusLine().getStatusCode() == 404)
-            {
-            	if (data.equals(API_ERROR_STEAM_USER_DOES_NOT_EXIST))
-            	{
-            		throw new APIException("Steam user does not exist");
-            	}
-            	else {
-            		throw new APIException("Unknown API exception; HTTP 404 error");
-            	}
-            }
-
-            ObjectMapper mapper = new ObjectMapper();
-            JSONArray jsonArray = new JSONArray(data);
-            ArrayList<SteamGame> games = new ArrayList<SteamGame>();
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                games.add(mapper.readValue(jsonArray.getJSONObject(i).toString(), SteamGame.class));
-            }
-
-            return games;
-        } catch (Exception e) {
-            if (e instanceof APIException) {
-            	throw e;
-            }
-        }
-		return null;
-    }
-    
-    public static List<RankEntryByTotalPlayTime> getRanksByTotalPlayTime(int fromRank, int toRank) {
-    	HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("http://localhost:6789/leaderboards?type=games&from=" + fromRank + "&to=" + toRank);
-        HttpResponse response = null;
+    public static List<SteamGame> getSteamGames(SteamRankingsClient client) throws JsonParseException, JsonMappingException, JSONException, IOException, APIException {
+        String data = client.excecuteRequest("games");
         
-        try {
-        	response = client.execute(request);
-        	String data = EntityUtils.toString(response.getEntity());
-        	
-        	ObjectMapper mapper = new ObjectMapper();
-        	JSONArray jsonArray = new JSONArray(data);
-        	ArrayList<RankEntryByTotalPlayTime> ranks = new ArrayList<RankEntryByTotalPlayTime>();
-        	
-        	for(int i = 0; i < jsonArray.length(); i++) {
-        		ranks.add(mapper.readValue(jsonArray.getJSONObject(i).toString(), RankEntryByTotalPlayTime.class));
-        	}
-        	
-        	return ranks;
-        } catch (Exception e) {
-        	return null;
-        }	
+        ObjectMapper mapper = new ObjectMapper();
+        JSONArray jsonArray = new JSONArray(data);
+        ArrayList<SteamGame> games = new ArrayList<SteamGame>();
+        
+        for(int i = 0; i < jsonArray.length(); i++) {
+            games.add(mapper.readValue(jsonArray.getJSONObject(i).toString(), SteamGame.class));
+        }
+        
+        return games;
+    }
+
+    public static List<RankEntryByTotalPlayTime> getRanksByTotalPlayTime(int fromRank, int toRank, SteamRankingsClient client) {
+        return null;
     }
 }
